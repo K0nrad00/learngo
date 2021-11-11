2f0ad8d33adf147ab08a0d64cfb0b51fc958ca7e
Konrad Private <konrad.wrobel@gmail.com>
2021-11-11 16:35:55 +0000

    02 my solution

---
+++ 02-write-your-first-program/exercises/01_my_solution.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("K0ndar00")
+	fmt.Println(`Friend named SOmeone`)
+
+	fmt.Print("konrad print")
+	fmt.Print("This ", " is", "multi Print\n")
+}
